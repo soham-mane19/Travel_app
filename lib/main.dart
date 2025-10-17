33182457ebd2ced7b2121f3f38121bddd086be91
soham-mane19 <fluttersohammane@gmail.com>
2025-10-17 23:06:28 +0530

    change the folder strucure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_app/Spalsh_Screen.dart';
-import 'package:travel_app/firebase_options.dart';
+import 'package:travel_app/firebase/firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
